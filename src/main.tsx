8e1cff27f41a4d34cd1009acee71c24f3a7ac7a0
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-07-20 09:17:33 +0200

    first commit home

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Header as GlobalHeader } from './App/common/index.tsx'
 import {Footer as GlobalFooter} from './App/common/index.tsx'
 import router from './router.tsx'
 import './main.scss'
-
+//
 
 createRoot(document.getElementById('root')!).render(
         <RouterProvider router={router}/>
